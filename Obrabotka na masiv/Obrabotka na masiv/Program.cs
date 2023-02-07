4d08d9f2e0de7b21577d062107e65bd908fdfd3b
Stefata05 <s.martinov2019@pmg-pernik.com>
2023-02-07 12:24:05 +0200

    Add file

---
+++ Obrabotka na masiv/Obrabotka na masiv/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Obrabotka_na_masiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine()); 
+            }
+            Console.WriteLine(OprtionsManager.CountLess100(arr));
+            Console.WriteLine(OprtionsManager.MaxNum(arr));
+            arr = OprtionsManager.ReverseArr(arr);
+            OprtionsManager.PrintArr(arr);
+        }
+    }
+}
