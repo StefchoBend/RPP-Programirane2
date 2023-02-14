4fd28338229d2c2154edb3fe9d0c44f787b82a81
Stefata05 <s.martinov2019@pmg-pernik.com>
2023-02-14 13:14:19 +0200

    add zadacha2

---
+++ ListTasks/SlivaneNaSpisuci/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SlivaneNaSpisuci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
