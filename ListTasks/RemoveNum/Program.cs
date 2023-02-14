a1b5eb1b439b54f5847e34e968272b65435a5a25
Stefata05 <s.martinov2019@pmg-pernik.com>
2023-02-14 13:00:25 +0200

    add zadacha1

---
+++ ListTasks/RemoveNum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace RemoveNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            var last = nums[nums.Count - 1];
+            //za indexa na poslediniq element
+            //nums.Remove(last);
+            nums.RemoveAll(x => x == last);
+            Console.WriteLine(String.Join(" ", nums));
+        }
+    }
+}
---
+++ Obrabotka na masiv/Zadacha1/OptionsManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha1
+{
+    public static class OptionsManager
+    {
+        public static string[] Reverse
+    }
+}
