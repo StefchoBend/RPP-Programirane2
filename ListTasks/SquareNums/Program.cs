5a359f92fddf5b6f277928a9771c5eb5084a94e2
Stefata05 <s.martinov2019@pmg-pernik.com>
2023-02-21 12:28:31 +0200

    add ListManipulation - skelet

---
+++ ListTasks/SquareNums/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SquareNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var result = nums.Where(x => Math.Sqrt(x) == (int)Math.Sqrt(x)).ToList();
+            Console.WriteLine(string.Join(" ", result));
+
+        }
+    }
+}
